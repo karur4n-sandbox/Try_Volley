34f14ea0f5e041e0d4898072d856633740f4638a
karur4n <karuran.f@gmail.com>
2015-01-18 02:22:35 +0900

    [Add] Add Volley Reqeust

--- app/src/main/java/com/example/karuran/try_volley/MainActivity.java
+++ app/src/main/java/com/example/karuran/try_volley/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.karuran.try_volley;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +20,31 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        request();
     }
+    
+    private void request() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.google.com";
 
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d("RESPONSE", response);
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", "That didn't work");
+            }
+        }
+        );
+        
+        queue.add(stringRequest);
+                
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
